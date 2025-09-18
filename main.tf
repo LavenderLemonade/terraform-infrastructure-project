d44fdc22e759efee090939ff016ffdb2d87cf5e0
Sam Elliott <soe1@uakron.edu>
2025-09-17 22:37:12 -0400

    realized i misnamed the subnetsso i renamed them and added the private subnet

--- main.tf
+++ main.tf
@@ -21,12 +21,21 @@ resource "aws_vpc" "main-vpc" {
     }
 }
 
-resource "aws_subnet" "main-vpc" {
+resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main-vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
   }
 }
 
