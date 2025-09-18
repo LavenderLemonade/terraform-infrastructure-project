77dabd0c3f1177d2dd4bb57a7b03df35d9a64dc4
Sam Elliott <soe1@uakron.edu>
2025-09-17 22:35:25 -0400

    updated the main vpc to have a specific CIDR range, and then added in the first subnet, the public one

--- main.tf
+++ main.tf
@@ -12,8 +12,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main-vpc" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "main-vpc"
+    }
+}
+
+resource "aws_subnet" "main-vpc" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
 
