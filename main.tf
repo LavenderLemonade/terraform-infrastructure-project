2f95dc4fbc5fb948d87a7068b67b45831a904a5b
Sam Elliott <soe1@uakron.edu>
2025-09-21 14:00:47 -0400

    FINALLY got the whole IAM roles and the S3 access figured out for the EC2 instance! this was uneccesarily confusing!

--- main.tf
+++ main.tf
@@ -56,6 +56,7 @@ resource "aws_instance" "web_server" {
   ami           = "ami-08982f1c5bf93d976" # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id # Associates the instance with the subnet
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   tags = {
     Name = "web-server"
   }
@@ -122,18 +123,59 @@ resource "aws_db_instance" "test_db" {
   skip_final_snapshot    = true
 }
 
-resource "aws_s3_bucket" "sammy-bucket-example" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "sammy-new-bucket-example" {
+  bucket = "my-tf-test-bucket-sammy"
   tags = {
-    Name        = "My bucket"
+    Name        = "sammy-new-bucket-sept-25"
     Environment = "Dev"
   }
 
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.sammy-bucket-example.id
+  bucket = aws_s3_bucket.sammy-new-bucket-example.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+data "aws_iam_policy_document" "ec2_trust_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "EC2S3AccessRole"
+  assume_role_policy = data.aws_iam_policy_document.ec2_trust_policy.json
+}
+
+data "aws_iam_policy_document" "s3_access_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.sammy-new-bucket-example.arn]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.sammy-new-bucket-example.arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "s3_access" {
+  name   = "S3AccessInlinePolicy"
+  role   = aws_iam_role.ec2_role.id
+  policy = data.aws_iam_policy_document.s3_access_policy.json
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2InstanceProfile"
+  role = aws_iam_role.ec2_role.name
 }
